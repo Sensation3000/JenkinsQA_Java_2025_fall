0324a6cde8d4943f6bbe48366bb99e1fa126d019
Stanislav Agafonov <chaltstas@yandex.ru>
2025-12-23 14:04:41 +0300

    RF_04.006.02.01 Перевод теста testAddAxisMenuOnlyUserDefined на POM

--- src/test/java/school/redrover/page/MultiConfigurationProjectConfigurationPage.java
+++ src/test/java/school/redrover/page/MultiConfigurationProjectConfigurationPage.java
@@ -1,10 +1,13 @@
 package school.redrover.page;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.List;
 
 
 public class MultiConfigurationProjectConfigurationPage extends BaseProjectConfigurationPage<MultiConfigurationProjectConfigurationPage> {
@@ -14,6 +17,12 @@ public class MultiConfigurationProjectConfigurationPage extends BaseProjectConfi
 
     @FindBy(id = "configuration-matrix")
     private WebElement configurationMatrix;
+
+    @FindBy(css = "[suffix=axis]")
+    private WebElement addAxisButton;
+
+    @FindBy(className = "jenkins-dropdown__item")
+    private List<WebElement> addAxisDropdownList;
 
 
     public MultiConfigurationProjectConfigurationPage(WebDriver driver) {
@@ -37,5 +46,19 @@ public class MultiConfigurationProjectConfigurationPage extends BaseProjectConfi
 
     public String getConfigurationMatrixText() {
         return getWait5().until(ExpectedConditions.visibilityOf(configurationMatrix)).getText().trim();
+    }
+
+    public MultiConfigurationProjectConfigurationPage clickAddAxisButton() {
+        ((JavascriptExecutor) getDriver())
+                .executeScript("arguments[0].scrollIntoView({block: 'center'});", addAxisButton);
+        getWait10().until(ExpectedConditions.elementToBeClickable(addAxisButton));
+        addAxisButton.click();
+
+        return this;
+    }
+
+    public List<String> getAddAxisDropdownItemText() {
+        getWait10().until(ExpectedConditions.visibilityOf(addAxisDropdownList.get(0)));
+        return addAxisDropdownList.stream().map(WebElement::getText).toList();
     }
 }
